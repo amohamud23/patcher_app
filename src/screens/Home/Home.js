7c12e6eb035e036e5026ed0e99098d3a7e84d36d
Abdi <amohamud23@gmail.com>
2021-03-17 01:12:35 -0700

    create Home Screen

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {} from "react-native";
+import Wrapper from "../../components/Wrapper/Wrapper";
+import Text from "../../components/Text/Text";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Wrapper>
+        <Text>Home</Text>
+      </Wrapper>
+    );
+  }
+}
+
+export default Home;
