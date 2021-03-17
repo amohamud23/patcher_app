faafae2a286c7d8b9f6c469f92d0eb052edb1fce
Abdi <amohamud23@gmail.com>
2021-03-17 01:11:27 -0700

    create settings screen

---
+++ src/screens/Settings/Settings.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import Text from "../../components/Text/Text";
+import Wrapper from "../../components/Wrapper/Wrapper";
+
+function Settings() {
+  return (
+    <Wrapper>
+      <TextInput>Settings</TextInput>
+    </Wrapper>
+  );
+}
+
+export default Settings;
