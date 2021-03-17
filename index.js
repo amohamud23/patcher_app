96d0a255d580dfa1c280ded3d291ad6c37be8f5e
Abdi <amohamud23@gmail.com>
2021-03-16 18:03:43 -0700

    wrapped around a redux store

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-import { registerRootComponent } from 'expo';
+import React from "react";
+import { registerRootComponent } from "expo";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import App from './App';
+import configureStore from "./src/store/configureStore";
+
+const { store, persistor } = configureStore();
+
+import App from "./App";
+
+function PatcherApp() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(PatcherApp);
