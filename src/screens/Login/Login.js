09d719372364d40ded87c3581242921d78166523
Abdi <amohamud23@gmail.com>
2021-03-16 21:18:48 -0700

    created Login screen file

---
+++ src/screens/Login/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+import Text from "../../components/Text/Text";
+import Wrapper from "../../components/Wrapper/Wrapper";
+
+function Login() {
+  return (
+    <Wrapper>
+      <View>
+        <Text>Login</Text>
+      </View>
+    </Wrapper>
+  );
+}
+
+export default Login;
