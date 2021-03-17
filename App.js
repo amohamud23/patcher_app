3b942f77a027f18a3f6183962614fe0fdf5759c2
Abdi <amohamud23@gmail.com>
2021-03-16 21:17:19 -0700

    added navigation and tabs and color theme

--- App.js
+++ App.js
@@ -1,21 +1,164 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, Switch, StatusBar } from "react-native";
+import Wrapper from "./src/components/Wrapper/Wrapper";
+import { changeTheme } from "./src/store/actions/appConfigActions";
+import { connect } from "react-redux";
+import Text from "./src/components/Text/Text";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Login from "./src/screens/Login/Login";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { colors, darkTheme, lightTheme } from "./src/constants/theme";
 
-export default function App() {
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tabs(props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            // eslint-disable-next-line prettier/prettier
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Wallet") {
+            iconName = focused ? "wallet" : "wallet-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person-circle" : "person-circle-outline";
+          } else if (route.name === "Community") {
+            iconName = focused ? "earth" : "earth-outline";
+          } else if (route.name === "Discover") {
+            iconName = focused ? "search" : "search-outline";
+          }
+
+          // You can return any component that you like here!
+          return (
+            <Ionicons
+              containerStyle={{ marginRight: 10 }}
+              name={iconName}
+              size={25}
+              color={color}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        style: {
+          backgroundColor:
+            props.theme === "light"
+              ? lightTheme.background
+              : darkTheme.background,
+        },
+        activeTintColor: colors.green,
+        inactiveTintColor: "gray",
+        showLabel: true,
+        tabStyle: {
+          padding: 2,
+        },
+        tabBar: {
+          backgroundColor: "red",
+          borderWidth: 2,
+          height: 32,
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 0,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={AuthStack}
+        options={{
+          headerShown: false,
+        }}
+        navigationOptions={{
+          gestureEnabled: false,
+        }}
+      />
+      <Tab.Screen
+        name="Home2"
+        component={AuthStack}
+        options={{
+          headerShown: false,
+        }}
+        navigationOptions={{
+          gestureEnabled: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+const AuthStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false,
+          gestureEnabled: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+function App(props) {
+  const [buttton, setButton] = useState(false);
+  const toggleSwitch = () => {
+    setButton(!buttton);
+    buttton ? props.setTheme("dark") : props.setTheme("light");
+  };
+
+  return (
+    // <Wrapper>
+    //   <StatusBar
+    //     barStyle={props.theme === "light" ? "dark-content" : "light-content"}
+    //   />
+    //   <View style={styles.container}>
+    //     <Text>Open up App.js to start working on your app!</Text>
+    //     <StatusBar style="auto" />
+    //     <Switch onValueChange={toggleSwitch} value={buttton} />
+    //   </View>
+    // </Wrapper>
+    <NavigationContainer>
+      <StatusBar
+        barStyle={props.theme === "light" ? "dark-content" : "light-content"}
+      />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Tabs}
+          options={{
+            headerShown: false,
+            gestureEnabled: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setTheme: (theme) => dispatch(changeTheme(theme)),
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    theme: state.appConfigReducer.theme,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
