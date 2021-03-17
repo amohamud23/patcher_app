26b6d455130c8f5d508b594195c8bb0d52624bf4
Abdi <amohamud23@gmail.com>
2021-03-17 01:13:13 -0700

    pass props to react native text

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { connect } from "react-redux";
+import { darkTheme, lightTheme } from "../../constants/theme";
 
 function FontText(props) {
+  const theme = props.theme === "light" ? lightTheme : darkTheme;
   return (
-    <Text
-      {...props}
-      style={[
-        props.style,
-        { color: props.theme === "light" ? "black" : "white" },
-      ]}
-    >
+    <Text {...props} style={[props.style, { color: theme.color }]}>
       {props.children}
     </Text>
   );
