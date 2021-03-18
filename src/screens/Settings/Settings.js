3b8b192fc76ffb1b1dee0b88825f4f4d8a7a9ecd
Abdi <amohamud23@gmail.com>
2021-03-17 19:25:13 -0700

    created a logout button

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -1,15 +1,61 @@
-import React from "react";
-import {} from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import React, { useState } from "react";
+import {
+  Switch,
+  TouchableWithoutFeedback,
+  View,
+  StyleSheet,
+} from "react-native";
+
+import { connect } from "react-redux";
 import Text from "../../components/Text/Text";
 import Wrapper from "../../components/Wrapper/Wrapper";
+import { colors } from "../../constants/theme";
+import { changeTheme } from "../../store/actions/appConfigActions";
+import { userLogout } from "../../store/actions/authAction";
 
-function Settings() {
+function Settings(props) {
+  const [buttton, setButton] = useState(false);
+  const toggleSwitch = () => {
+    setButton(!buttton);
+    buttton ? props.setTheme("dark") : props.setTheme("light");
+  };
+  const handleLogout = () => {
+    const { navigation, onLogout } = props;
+    onLogout();
+    navigation.navigate("Login");
+  };
   return (
     <Wrapper>
-      <TextInput>Settings</TextInput>
+      <Text>Settings</Text>
+      <Switch onValueChange={toggleSwitch} value={buttton} />
+      <TouchableWithoutFeedback onPress={handleLogout}>
+        <View style={styles.logoutBtn}>
+          <Text>Logout</Text>
+        </View>
+      </TouchableWithoutFeedback>
     </Wrapper>
   );
 }
 
-export default Settings;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setTheme: (theme) => dispatch(changeTheme(theme)),
+    onLogout: () => dispatch(userLogout()),
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    theme: state.appConfigReducer.theme,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
+
+const styles = StyleSheet.create({
+  logoutBtn: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: colors.grey,
+  },
+});
