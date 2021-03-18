4c7add9c34f9e39a38329ea36cadb11e52e4cc5f
Abdi <amohamud23@gmail.com>
2021-03-17 19:25:02 -0700

    new routes screen

---
+++ src/screens/Routes/Routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {} from "react-native";
+import Wrapper from "../../components/Wrapper/Wrapper";
+import Text from "../../components/Text/Text";
+
+function Routes(props) {
+  return (
+    <Wrapper>
+      <Text>Routes</Text>
+    </Wrapper>
+  );
+}
+
+export default Routes;
