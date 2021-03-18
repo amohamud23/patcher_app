2fcc742d797acb38ead951b46a95b18e15536bc0
Abdi <amohamud23@gmail.com>
2021-03-18 16:29:31 -0700

    new action types

--- src/store/actions/ActionTypes.js
+++ src/store/actions/ActionTypes.js
@@ -2,3 +2,7 @@ export const AUTH_SUCCESS = "AUTH_SUCCESS";
 export const AUTH_FAIL = "AUTH_FAIL";
 export const CHANGE_THEME = "CHANGE_THEME";
 export const AUTH_LOGOUT = "AUTH_LOGOUT";
+export const GET_INBOX = "GET_INBOX";
+export const GET_ROUTES = "GET_ROUTES";
+export const COMPLETED_ROUTE = "COMPLETED_ROUTE";
+export const SENT_MESSAGE = "SENT_MESSAGE";
